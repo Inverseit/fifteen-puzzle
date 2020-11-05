cf40e50da190878e0fd1e2705f8fb457bb82a38a
seitkali_u <seitkalievulan@gmail.com>
2020-11-05 21:39:42 +0300

    Add solver class

--- solve.py
+++ solve.py
@@ -5,18 +5,24 @@ from copy import deepcopy
 from gamelogic import GameLogic
 
 class Positions(object):
-    def __init__(self, lvl, board, empty, path):
-        self.n = 4
-        self.goalString = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0,"
-        self.goal = [[1, 2, 3, 4], [5, 6, 7, 8],
-                     [9, 10, 11, 12], [13, 14, 15, 0]]
+    def __init__(self, lvl, board, empty, path,n):
+        self.n = n
+        self.goal = self.getGoalBoard()
+        self.goalString = self.getHashString(board = self.goal)
         self.level = lvl
         self.board = board
         self.empty = empty
         self.path = path
 
-        self.getHashString()
+        self.getHashString(self.board)
         self.calculateCost()
+
+    def getGoalBoard(self):
+        board = []
+        sz = self.n
+        board = [[j for j in range(i*sz+1, (i+1)*sz+1)] for i in range(sz)]
+        board[-1][-1] = 0  # make the last element 0 (empty)
+        return board
 
     def calculateCost(self):
         self.cost = self.level + self.manhDist()
@@ -35,11 +41,11 @@ class Positions(object):
         board[-1][-1] = 0  # make the last element 0 (empty)
         return board
     
-    def getHashString(self):
+    def getHashString(self, board):
         hashString = ""
         for i in range(self.n):
             for j in range(self.n):
-                hashString += (str(self.board[i][j]) + ",")
+                hashString += (str(board[i][j]) + ",")
         self.hashString = hashString
         return hashString 
 
@@ -48,7 +54,7 @@ class Positions(object):
         return self.hashString == self.goalString
 
     def __hash__(self):
-        return hash( self.hashString)
+        return hash(self.hashString)
 
     def possibleMoves(self):
         # moves = [(-1, 0, "U"), (1, 0, "D"), (0, -1, "L"), (0, 1, "R")]
@@ -86,44 +92,52 @@ class Positions(object):
         return tuple(map(lambda i, j: i + j, t1, t2))
 
 
-def main():
+class Solver():
+    def __init__(self, g, n):
+        self.n = n
+        self.start = time.time()
+        self.g = g
+        self.g.shuffleBoard()
+        self.board = g.getBoard()
+        self.empty = (n-1, n-1)
+        self.queue = PriorityQueue()
+        self.visited = set()
 
-    start = time.time()
-    g = GameLogic(4)
-    g.shuffleBoard()
-    board = g.getBoard()
-    print(board)
-    empty = (3, 3)
-    start = time.time()
-    path = ""
-    queue = PriorityQueue()
-    visited = set()
-    level = 0
-    root = Positions(level, board, empty, path)
+    def getSolution(self):
+        return self.solve()
 
-    queue.put(root)
-    i = 0
-    while not queue.empty():
-        pos = queue.get()
+    def solve(self):
+        root = Positions(0, self.board, self.empty, "", self.n)
+        self.queue.put(root)
 
-        if i % 1000 == 0:
-            print("checking " + str(i) + "th position, level is ", str(pos.level), "cost is ", str(pos.cost))
-        i += 1
+        i = 0
+        while not self.queue.empty():
+            pos = self.queue.get()
 
-        if pos.level > 70:
-            solution = "No under 70"
-            break
+            if i % 5000 == 0:
+                print("checking " + str(i) + "th position, level is ",
+                    str(pos.level), "cost is ", str(pos.cost))
+            i += 1
 
-        if pos.isSolved():
-            solution = pos.path
-            break
+            if pos.level > 70:
+                solution = "No under 70"
+                break
 
-        for newBoard, newEmpty, move in pos.possibleMoves():
-            newPos = Positions(pos.level+1, newBoard, newEmpty, pos.path + move)
-            if newPos not in visited:
-                visited.add(newPos)
-                queue.put(newPos)
-    print(time.time() - start, solution)
+            if pos.isSolved():
+                solution = pos.path
+                break
+
+            for newBoard, newEmpty, move in pos.possibleMoves():
+                newPos = Positions(pos.level+1, newBoard,
+                                newEmpty, pos.path + move, self.n)
+                if newPos not in self.visited:
+                    self.visited.add(newPos)
+                    self.queue.put(newPos)
+        print(time.time() - self.start, solution)
+        return solution
+
 
 if __name__ == "__main__":
-    main()
+    g = GameLogic(4)
+    soln = Solver(g, 4).getSolution()
+    print(soln)
